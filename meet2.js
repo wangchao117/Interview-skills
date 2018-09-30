818fd8906b77dc627d336edd1f0bff36467cef64
chao.wang <wcd126yx@126.com>
2018-09-30 15:12:32 +0800

    css3新特性

--- meet2.js
+++ meet2.js
@@ -74,7 +74,104 @@
   两种盒子模型：IE盒模型和标准盒模型，都包含几个部分：content(内容),padding(内边距)，border(边框)，margin(外边距)
   注意：IE盒模型的content会把padding和border算进去
 
-11.
+11.css优先级
+  优先级就近原则，同权重情况下样式定义最近者为准;
+  载入样式以最后载入的定位为准;
+  优先级为:
+       同权重: 内联样式表（标签内部）> 嵌入样式表（当前文件中）> 外部样式表（外部文件中）。
+       !important >  id > class > tag
+       important 比 内联优先级高
+
+12.css3新特性
+  新增一些CSS3选择器，如div>p，p:first-of-type，p:last-of-type，p:nth-of-type(2)
+  CSS3 边框（Borders）： border-image	设置所有边框图像的速记属性
+                        border-radius	一个用于设置所有四个边框- *-半径属性的速记属性
+                        box-shadow	附加一个或多个下拉框的阴影
+  CSS3 背景： background-clip	规定背景的绘制区域。
+             background-origin	规定背景图片的定位区域。
+             background-size	规定背景图片的尺寸。
+  渐变。CSS3 定义了两种类型的渐变（gradients）：
+            线性渐变（Linear Gradients）- 向下/向上/向左/向右/对角方向
+            径向渐变（Radial Gradients）- 由它们的中心定义
+  CSS3 文本效果：比如阴影等
+  CSS3 转换和变形：transform：适用于2D或3D转换的元素; transform-origin；允许您更改转化元素位置
+      2D新转换属性：
+        matrix(n,n,n,n,n,n) 定义 2D 转换，使用六个值的矩阵。
+        translate(x,y) 定义 2D 转换，沿着 X 和 Y 轴移动元素。
+        translateX(n) 定义 2D 转换，沿着 X 轴移动元素。
+        translateY(n) 定义 2D 转换，沿着 Y 轴移动元素。
+        scale(x,y) 定义 2D 缩放转换，改变元素的宽度和高度。
+        scaleX(n) 定义 2D 缩放转换，改变元素的宽度。
+        scaleY(n) 定义 2D 缩放转换，改变元素的高度。
+        rotate(angle) 定义 2D 旋转，在参数中规定角度。
+        skew(x-angle,y-angle) 定义 2D 倾斜转换，沿着 X 和 Y 轴。
+        skewX(angle) 定义 2D 倾斜转换，沿着 X 轴。
+        skewY(angle) 定义 2D 倾斜转换，沿着 Y 轴。
+      3D转换属性：
+        transform 向元素应用 2D 或 3D 转换。
+        transform-origin 允许你改变被转换元素的位置。
+        transform-style  规定被嵌套元素如何在 3D 空间中显示。
+        perspective 规定 3D 元素的透视效果。
+        perspective-origin 规定 3D 元素的底部位置。
+        backface-visibility 定义元素在不面对屏幕时是否可见。
+      3D 转换方法：
+        matrix3d(n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n) 定义 3D 转换，使用 16 个值的 4x4 矩阵。
+        translate3d(x,y,z) 定义 3D 转化。
+        translateX(x) 定义 3D 转化，仅使用用于 X 轴的值。
+        translateY(y) 定义 3D 转化，仅使用用于 Y 轴的值。
+        translateZ(z) 定义 3D 转化，仅使用用于 Z 轴的值。
+        scale3d(x,y,z) 定义 3D 缩放转换。
+        scaleX(x) 定义 3D 缩放转换，通过给定一个 X 轴的值。
+        scaleY(y) 定义 3D 缩放转换，通过给定一个 Y 轴的值。
+        scaleZ(z) 定义 3D 缩放转换，通过给定一个 Z 轴的值。
+        rotate3d(x,y,z,angle) 定义 3D 旋转。
+        rotateX(angle)	定义沿 X 轴的 3D 旋转。
+        rotateY(angle)	定义沿 Y 轴的 3D 旋转。
+        rotateZ(angle)	定义沿 Z 轴的 3D 旋转。
+        perspective(n)	定义 3D 转换元素的透视视图。
+  CSS3 过渡,过渡属性:
+        transition 简写属性，用于在一个属性中设置四个过渡属性。
+        transition-property 规定应用过渡的 CSS 属性的名称。
+        transition-duration 定义过渡效果花费的时间。默认是 0。
+        transition-timing-function 规定过渡效果的时间曲线。默认是 "ease"。
+        transition-delay 规定过渡效果何时开始。默认是 0。
+  css3动画：要创建CSS3动画，你需要了解@keyframes规则。@keyframes规则是创建动画。 @keyframes规则内指定一个CSS样式和动画将逐步从目前的样式更改为新的样式。
+  CSS3 盒模型
+  CSS3伸缩布局盒模型(弹性盒)
+
+13.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+本文来自 其實很簡單 的CSDN 博客 ，全文地址请点击：https://blog.csdn.net/chandoudeyuyi/article/details/69206236?utm_source=copy
+
+
+
+
+
+
+
+
+
 
 
 
